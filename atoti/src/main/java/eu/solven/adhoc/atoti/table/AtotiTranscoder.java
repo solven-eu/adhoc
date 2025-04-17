7458959c26e3e9838d9d07358bed748758b35d43
Benoit Lacelle <benoit.lacelle@gmail.com>
2025-04-17 12:36:03 +0400

    Add item in roadmap

--- atoti/src/main/java/eu/solven/adhoc/atoti/table/AtotiTranscoder.java
+++ atoti/src/main/java/eu/solven/adhoc/atoti/table/AtotiTranscoder.java
@@ -28,9 +28,9 @@ import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * This {@link ITableTranscoder} is useful into translating from levelNames as configured in ActivePivot
- * processors, into fieldName used in underlying {@link eu.solven.adhoc.table.ITableWrapper}. It assumes the fieldName
- * matched the levelName.
+ * This {@link ITableTranscoder} is useful into translating from levelNames as configured in ActivePivot processors,
+ * into fieldName used in underlying {@link eu.solven.adhoc.table.ITableWrapper}. It assumes the fieldName matched the
+ * levelName.
  *
  * This is useful when loading an ActivePivot configuration (e.g. transcoded from ActivePivot postprocessor properties).
  * But this is not specific to querying ActivePivot as an Adhoc Database.
