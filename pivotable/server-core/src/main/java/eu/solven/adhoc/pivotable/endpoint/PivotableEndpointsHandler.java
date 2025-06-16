732a8b4083c9966ee9688dccbff90de7b896687a
Benoit Lacelle <benoit.lacelle@gmail.com>
2025-06-16 12:22:30 +0400

    Fix spotless

--- pivotable/server-core/src/main/java/eu/solven/adhoc/pivotable/endpoint/PivotableEndpointsHandler.java
+++ pivotable/server-core/src/main/java/eu/solven/adhoc/pivotable/endpoint/PivotableEndpointsHandler.java
@@ -23,15 +23,11 @@
 package eu.solven.adhoc.pivotable.endpoint;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
